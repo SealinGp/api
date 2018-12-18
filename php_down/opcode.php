9389a1f12c2a126f80630f101821e71359831448
sea <v_kenqzhang@tencent.com>
2018-12-18 19:05:09 +0800

    更新

--- php_down/opcode.php
+++ php_down/opcode.php
@@ -14,12 +14,37 @@
  * 结构体:
  *      struct _zend_op {
             opcode_handler_t handler;       //执行该opcode时调用的处理函数
-            znode result;
+            znode result;                   //保存该指令,执行完毕后的结果
             znode op1;
             znode op2;
             ulong extended_value;
             uint lineno;
             zend_uchar opcode;  //opcode代码
  *      }
+ *
+ * 例子
+ * ej:
+ * print 函数/语句 有设置返回值相关信息result
+        void zend_do_print(znode *result，const znode *arg TSRMLS_DC)
+        {
+            zend_op *opline = get_next_op(CG(active_op_array) TSRMLS_CC);   //创建一个实例zend_op
+            
+            opline->result.op_type = IS_TMP_VAR;                            //返回值类型设置为临时变量
+            opline->result.u.var = get_temporary_variable(CG(active_op_array));
+            opline->opcode = ZEND_PRINT;
+            opline->op1 = *arg;
+            SET_UNUSED(opline->op2);
+             *result = opline->result;
+        }
+ *
+ * echo 语句 没有设置返回值相关信息result
+        void zend_do_echo(const znode *arg TSRMLS_DC)
+        {
+            zend_op *opline = get_next_op(CG(active_op_array) TSRMLS_CC);
+            
+            opline->opcode = ZEND_ECHO;
+            opline->op1 = *arg;                                             //输入参数
+            SET_UNUSED(opline->op2);
+        }
  *
 */
