a5a4083f3f0cc2acc6adf8a15e059022d30a5c10
sea <v_kenqzhang@tencent.com>
2019-01-04 09:58:01 +0800

    update

--- autoload.php
+++ autoload.php
@@ -3,11 +3,13 @@ header('content-type:application/json');
 define('ROOT', realpath(substr(__DIR__, 0, -4)));
 spl_autoload_register('load');
 $psr4 = [
-    'wsdl' => 'API\\RequestMethod\\soap\\wsdl'
+    'wsdl' => 'API\\RequestMethod\\soap\\wsdl',
+    'API'  => 'api'
 ];
 define('PSR4',$psr4);
 function load(string $lib): void
 {
+    
     foreach (PSR4 as $k => $v) {
         false !== stripos($lib,$k) && $lib = strtr($lib,[$k=>$v]);
     }
