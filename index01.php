a07e71ee5382dab762e433b76671c5a4a3f6804a
sea <v_kenqzhang@tencent.com>
2019-05-14 18:02:11 +0800

    code clean

--- index01.php
+++ index01.php
@@ -1,22 +1,8 @@
 <?php
 header("Content-type:application/json;charset=utf-8");
-include '../functions.php';
-$a = '{
-	"Hs-CRP": [
-     {
-          "name":"1" ,
-          "model":"0x0001",
-          "val" : ""
-      },
-     {
-          "name":"2" ,
-          "mdoel":"0x0002",
-          "val" :{
-                     "reg":50,
-                     "speed":15
-                  }
-      }
-    ]
-}';
-$a = json_decode($a,true);
-var_dump($a);exit;
+//include '../functions.php';
+include 'autoload.php';
+$xml = new \API\ext\xml('./1.xml');
+$arr = $xml->to_arr('');
+
+var_dump($arr);exit;
